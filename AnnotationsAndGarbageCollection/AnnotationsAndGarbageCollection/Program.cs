b583f3c7863733f6041bee3e133e23da7d16c014
Reshma2310 <dreshma253@gmail.com>
2022-06-09 16:31:11 +0500

    [Reshma] - Executed Annotations Demo Program

---
+++ AnnotationsAndGarbageCollection/AnnotationsAndGarbageCollection/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AnnotationsAndGarbageCollection
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            NewAttribute.AttributeDisplay(typeof(Employer));
+            Console.WriteLine();
+            NewAttribute.AttributeDisplay(typeof(Employee));
+            Console.WriteLine();
+        }
+    }
+}
