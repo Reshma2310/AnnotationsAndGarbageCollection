a3a7e948cef09f50423c686e128b61ff1b714c42
Reshma2310 <dreshma253@gmail.com>
2022-06-09 17:04:20 +0500

    [Reshma] - Executed Garbage Collection Demo Program

---
+++ AnnotationsAndGarbageCollection/GarbageCollection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GarbageCollection
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            long mem1 = GC.GetTotalMemory(false);
+            {
+                int[] values = new int[50000];//Allocate an Array and make it unreachable
+                values = null;
+            }
+            long mem2 = GC.GetTotalMemory(false);
+            {
+                GC.Collect();//Collect Gargare
+            }
+            long mem3 = GC.GetTotalMemory(false);
+            {
+                Console.WriteLine(mem1);
+                Console.WriteLine(mem2);
+                Console.WriteLine(mem3);
+            }
+        }
+    }
+}
